459623a9db6945864f429371232bda3ca69b9259
cj1341392250 <137193211+cj1341392250@users.noreply.github.com>
2023-06-21 15:03:49 +0800

    Add files via upload

---
+++ three class/3_class_script.R
@@ -0,0 +1,184 @@
+setwd("G:/R/project/project6")
+library(nnet)
+library(data.table)
+require(questionr)
+require(DALEX)
+require(iBreakDown)
+require(caret)
+require(glmnet)
+library(pROC)
+train<-read.csv('3_class_input_train.csv')
+
+test<-read.csv('3classtest.csv')
+colnames(train)
+#event=1
+rt<-train
+rt$label<-as.numeric(rt$label)
+rt$label[rt$label==2]=0
+rt$label<-factor(rt$label)
+fit.full<-glm(label~.,data=rt,family = binomial)
+#fit.full<-multinom(label~.,data=train, probabilities = TRUE, model = TRUE)
+fit.result<-summary(fit.full)
+#计算p值
+# z <- summary(fit.full)$coefficients/summary(fit.full)$standard.errors
+# p <- (1 - pnorm(abs(z), 0, 1))*2
+# p
+#计算OR值
+#lable=1
+train_logstic<-round(odds.ratio(fit.full),2)
+fwrite(train_logstic,file = "Logstic_OR_1.csv",
+          quote = F,row.names = T) #多因素logstic分析结果保存
+
+
+
+
+
+
+
+rm(list = ls())
+#----2.计算ROC值------
+library(pROC)
+#---label=1---
+
+rt<-train
+rt$label<-as.numeric(rt$label)
+rt$label[rt$label==2]=0
+rt$label<-factor(rt$label)
+fit.full<-glm(label~.,data=rt,family = binomial)
+
+train_pre<-predict(fit.full,newdata = rt)
+train_roc<-roc(rt$label,train_pre,ci=T)
+best=coords(train_roc,'best',ret='all',transpose=F)
+
+rt2<-test
+rt2$label[rt2$label==2]=0
+test_pre<-predict(fit.full,newdata = test,terms = 'response') 
+test_roc<-roc(rt2$label,test_pre,ci=T)
+best_2=coords(test_roc,'best',ret='all',transpose=F)
+roc1_train<- roc(rt$label,train_pre,ci=T)
+roc1_test<-roc(rt2$label,test_pre,ci=T)
+outTab<-data.frame(
+  label=c('train_1'),
+  AUC=round(train_roc$auc,3),
+  '95% CI'=paste(round(train_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best$threshold,3),
+  spensitivity=round(best$specificity,3),
+  sensitivity=round(best$sensitivity,3),
+  accuracy=round(best$accuracy,3),
+  check.names = F)
+outTab2<-data.frame(
+  label=c('test_1'),
+  AUC=round(test_roc$auc,3),
+  '95% CI'=paste(round(test_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best_2$threshold,3),
+  spensitivity=round(best_2$specificity,3),
+  sensitivity=round(best_2$sensitivity,3),
+  accuracy=round(best_2$accuracy,3),
+  check.names = F)
+#--label=2---
+rt<-train
+rt$label<-as.numeric(rt$label)
+rt$label[rt$label==1]=0
+rt$label<-factor(rt$label)
+fit.full<-glm(label~.,data=rt,family = binomial)
+
+train_pre<-predict(fit.full,newdata = rt)
+train_roc<-roc(rt$label,train_pre,ci=T)
+best=coords(train_roc,'best',ret='all',transpose=F)
+
+rt2<-test
+rt2$label[rt2$label==1]=0
+test_pre<-predict(fit.full,newdata = test) 
+test_roc<-roc(rt2$label,test_pre,ci=T)
+best_2=coords(test_roc,'best',ret='all',transpose=F)
+roc2_train<- roc(rt$label,train_pre,ci=T)
+roc2_test<-roc(rt2$label,test_pre,ci=T)
+outTab3<-data.frame(
+  label=c('train_2'),
+  AUC=round(train_roc$auc,3),
+  '95% CI'=paste(round(train_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best$threshold,3),
+  spensitivity=round(best$specificity,3),
+  sensitivity=round(best$sensitivity,3),
+  accuracy=round(best$accuracy,3),
+  check.names = F)
+outTab4<-data.frame(
+  label=c('test_2'),
+  AUC=round(test_roc$auc,3),
+  '95% CI'=paste(round(test_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best_2$threshold,3),
+  spensitivity=round(best_2$specificity,3),
+  sensitivity=round(best_2$sensitivity,3),
+  accuracy=round(best_2$accuracy,3),
+  check.names = F)
+#----label=0---
+rt<-train
+rt$label<-as.numeric(rt$label)
+rt$label[rt$label==2]=1
+rt$label<-factor(rt$label,levels = c(1,0))
+
+fit.full<-glm(label~.,data=rt,family = binomial)
+
+train_pre<-predict(fit.full,newdata = rt)
+train_roc<-roc(rt$label,train_pre,ci=T)
+best=coords(train_roc,'best',ret='all',transpose=F)
+
+rt2<-test
+rt2$label[rt2$label==2]=1
+rt2$label<-factor(rt2$label,levels = c(1,0))
+
+test_pre<-predict(fit.full,newdata = test,terms = 'response') 
+test_roc<-roc(rt2$label,test_pre,ci=T)
+best_2=coords(test_roc,'best',ret='all',transpose=F)
+roc3_train<- roc(rt$label,train_pre,ci=T)
+roc3_test<-roc(rt2$label,test_pre,ci=T)
+outTab5<-data.frame(
+  label=c('train_0'),
+  AUC=round(train_roc$auc,3),
+  '95% CI'=paste(round(train_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best$threshold,3),
+  spensitivity=round(best$specificity,3),
+  sensitivity=round(best$sensitivity,3),
+  accuracy=round(best$accuracy,3),
+  check.names = F)
+outTab6<-data.frame(
+  label=c('test_0'),
+  AUC=round(test_roc$auc,3),
+  '95% CI'=paste(round(test_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best_2$threshold,3),
+  spensitivity=round(best_2$specificity,3),
+  sensitivity=round(best_2$sensitivity,3),
+  accuracy=round(best_2$accuracy,3),
+  check.names = F)
+whole_table<-rbind(outTab,outTab2,outTab3,outTab4,outTab5,outTab6)
+
+
+fwrite(whole_table,'Logstic_ROC.csv')
+
+
+save(roc1_test,roc1_train,roc2_test,roc2_train,roc3_test,roc3_train,file = 'ROCplot.Rdata')
+#----3.绘图----
+rm(list = ls())
+load('ROCplot.Rdata')
+pdf('ROC_train.pdf',width = 5,height = 5)
+plot.roc(roc3_train,col='red',identity.col = 'grey',legacy.axes = T)
+plot.roc(roc1_train,col='green',legacy.axes = T,add = T)
+plot.roc(roc2_train,col='blue',add = T,legacy.axes = T)
+legend('bottomright',c('0 vs Rest (AUC=0.802)','1 vs Rest (AUC=0.706)','2 vs Rest (AUC=0.894)'),col = c('red','green','blue'),lwd=2,bty='n')
+dev.off()
+
+pdf('ROC_test.pdf',width = 5,height = 5)
+plot.roc(roc3_test,col='red',identity.col = 'grey',legacy.axes = T)
+plot.roc(roc1_test,col='green',legacy.axes = T,add = T)
+plot.roc(roc2_test,col='blue',add = T,legacy.axes = T)
+legend('bottomright',c('0 vs Rest (AUC=0.762)','1 vs Rest (AUC=0.635)','2 vs Rest (AUC=0.784)'),col = c('red','green','blue'),lwd=2,bty='n')
+dev.off()
+
+#---4.delong test----
+test_12<-roc.test(roc1_test,roc2_test,method='delong')
+test_02<-roc.test(roc3_test,roc2_test,method='delong')
+test_01<-roc.test(roc1_test,roc3_test,method='delong')
+
+train_12<-roc.test(roc1_train,roc2_train,method='delong')
+train_02<-roc.test(roc3_train,roc2_train,method='delong')
+train_01<-roc.test(roc1_train,roc3_trai,method='delong')
---
+++ two class/Task 3_script.R
@@ -0,0 +1,482 @@
+rm(list = ls())
+library(nnet)
+library(data.table)
+require(questionr)
+require(DALEX)
+require(iBreakDown)
+library(dplyr)
+require(caret)
+require(glmnet)
+library(pROC)
+#----Select best seed----
+df <- read.csv('Task 3.csv')
+samples <- createDataPartition(df$label, p=0.7, list=FALSE)
+train<-df[samples,]
+test<-df[-samples,]
+fit.full<-glm(label~.,data=train,family = binomial)
+fit.result<-summary(fit.full)
+#AUC
+uniTab1<-data.frame()
+Sample <- list()
+
+for (i in 1:2000) {
+  set.seed(i) 
+  if(T){
+    Train <- createDataPartition(df$label, p=0.7, list=FALSE)
+    Sample[[i]]<- Train
+    train<-df[Train,]
+    test<-df[-Train,]
+    train$label<-factor(train$label)
+    class(train)
+    train<-as.data.frame(train)
+    fit.full<-glm(label~.,data=train,family = 'binomial')
+    fit.result<-summary(fit.full)
+    #计算p值
+    # z <- summary(fit.full)$coefficients/summary(fit.full)$standard.errors
+    # p <- (1 - pnorm(abs(z), 0, 1))*2
+    # p
+    #计算OR值
+    #lable=1
+    train_logstic<-round(odds.ratio(fit.full),2)
+    #AUC
+    train_pre<-predict(fit.full,newdata = train)
+    train_pre<-as.numeric(train_pre)
+    train_roc<-roc(train$label,train_pre,ci=T,plot=F)
+    best=coords(train_roc,'best',ret='all',transpose=F)
+    test_pre<-predict(fit.full,newdata = test,terms = 'response') 
+    test_roc<-roc(test$label,test_pre,ci=T,plot=F)
+    best_2=coords(test_roc,'best',ret='all',transpose=F)
+    uniTab1<-rbind(
+      uniTab1,
+      cbind(seed=i,trainAUC=round(train_roc$auc,3),testAUC=round(test_roc$auc,3))
+    )
+    
+  }
+}
+seed <- uniTab1 %>% 
+  dplyr::filter(trainAUC>0.90&testAUC>0.90,trainAUC>testAUC) %>% 
+  arrange(desc(trainAUC),desc(testAUC))
+#seed <- seed$seed[1];seed
+save(Sample,seed,file = 'model3_sample.Rdata')
+#---Established Model------
+rm(list = ls())
+load('model3_sample.Rdata')
+samples <- Sample[[1324]]
+set.seed(1324)
+if(T){
+df <- read.csv('Task 3.csv')
+df$AFP <- as.numeric(df$AFP)
+df$margin <- as.numeric(df$margin)
+df$label <- as.numeric(df$label)
+
+#AP_model----
+train_AP <- df %>% 
+  select(label,starts_with('AP_')) 
+train_AP <- train_AP[samples,]
+test_AP<-df %>% 
+  select(label,starts_with('AP_')) 
+test_AP <- test_AP[-samples,]
+train.control<-trainControl(method= "repeatedcv",  number= 10,repeats= 3)
+model<-train(label~.,data=train_AP,method= "lm",  trControl=train.control)
+AP_model<-glm(label~.,data=train_AP,family = binomial)
+train_AP_pre<-predict(AP_model,newdata = train_AP)
+train_AP_roc<-roc(train_AP$label,train_AP_pre,ci=T)
+best=coords(train_AP_roc,'best',ret='all',transpose=F)
+test_AP_pre<-predict(AP_model,newdata = test_AP,terms = 'response') 
+test_AP_roc<-roc(test_AP$label,test_AP_pre,ci=T)
+best_2=coords(test_AP_roc,'best',ret='all',transpose=F)
+AP_model_train<-data.frame(
+  Type='AP_model_train',
+  AUC=round(train_AP_roc$auc,3),
+  '95% CI'=paste(round(train_AP_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best$threshold,3),
+  spensitivity=round(best$specificity,3),
+  sensitivity=round(best$sensitivity,3),
+  accuracy=round(best$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+AP_model_test<-data.frame(
+  Type='AP_model_test',
+  AUC=round(test_AP_roc$auc,3),
+  '95% CI'=paste(round(test_AP_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best_2$threshold,3),
+  spensitivity=round(best_2$specificity,3),
+  sensitivity=round(best_2$sensitivity,3),
+  accuracy=round(best_2$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+
+#PVP_model----
+train_PVP <- df %>% 
+  select(label,starts_with('PVP_')) 
+train_PVP <- train_PVP[samples,]
+test_PVP<-df %>% 
+  select(label,starts_with('PVP_')) 
+test_PVP <- test_PVP[-samples,]
+train.control<-trainControl(method= "repeatedcv",  number= 10,repeats= 3)
+model<-train(label~.,data=train_PVP,method= "lm",  trControl=train.control)
+PVP_model<-glm(label~.,data=train_PVP,family = binomial)
+train_PVP_pre<-predict(PVP_model,newdata = train_PVP)
+train_PVP_roc<-roc(train_PVP$label,train_PVP_pre,ci=T)
+best=coords(train_PVP_roc,'best',ret='all',transpose=F)
+test_PVP_pre<-predict(PVP_model,newdata = test_PVP,terms = 'response') 
+test_PVP_roc<-roc(test_PVP$label,test_PVP_pre,ci=T)
+best_2=coords(test_PVP_roc,'best',ret='all',transpose=F)
+
+PVP_model_train<-data.frame(
+  Type='PVP_model_train',
+  AUC=round(train_PVP_roc$auc,3),
+  '95% CI'=paste(round(train_PVP_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best$threshold,3),
+  spensitivity=round(best$specificity,3),
+  sensitivity=round(best$sensitivity,3),
+  accuracy=round(best$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+PVP_model_test<-data.frame(
+  Type='PVP_model_test',
+  AUC=round(test_PVP_roc$auc,3),
+  '95% CI'=paste(round(test_PVP_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best_2$threshold,3),
+  spensitivity=round(best_2$specificity,3),
+  sensitivity=round(best_2$sensitivity,3),
+  accuracy=round(best_2$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+#T2_model----
+train_T2 <- df %>% 
+  select(label,starts_with('T2_')) 
+train_T2 <- train_T2[samples,]
+test_T2<-df %>% 
+  select(label,starts_with('T2_')) 
+test_T2 <- test_T2[-samples,]
+train.control<-trainControl(method= "repeatedcv",  number= 10,repeats= 3)
+model<-train(label~.,data=train_T2,method= "lm",  trControl=train.control)
+T2_model<-glm(label~.,data=train_T2,family = binomial)
+train_T2_pre<-predict(T2_model,newdata = train_T2)
+train_T2_roc<-roc(train_T2$label,train_T2_pre,ci=T)
+best=coords(train_T2_roc,'best',ret='all',transpose=F)
+test_T2_pre<-predict(T2_model,newdata = test_T2,terms = 'response') 
+test_T2_roc<-roc(test_T2$label,test_T2_pre,ci=T)
+best_2=coords(test_T2_roc,'best',ret='all',transpose=F)
+T2_model_train<-data.frame(
+  Type='T2_model_train',
+  AUC=round(train_T2_roc$auc,3),
+  '95% CI'=paste(round(train_T2_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best$threshold,3),
+  spensitivity=round(best$specificity,3),
+  sensitivity=round(best$sensitivity,3),
+  accuracy=round(best$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+T2_model_test<-data.frame(
+  Type='T2_model_test',
+  AUC=round(test_T2_roc$auc,3),
+  '95% CI'=paste(round(test_T2_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best_2$threshold,3),
+  spensitivity=round(best_2$specificity,3),
+  sensitivity=round(best_2$sensitivity,3),
+  accuracy=round(best_2$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+
+#Clinical_model----
+train_Clin <- df %>% 
+  select(-starts_with('PVP')) %>% 
+  select(-starts_with('T2')) %>% 
+  select(-starts_with('AP')) 
+train_Clin <- train_Clin[samples,]
+test_Clin<-df %>% 
+  select(-starts_with('PVP')) %>% 
+  select(-starts_with('T2')) %>% 
+  select(-starts_with('AP')) 
+test_Clin <- test_Clin[-samples,]
+train.control<-trainControl(method= "repeatedcv",  number= 10,repeats= 3)
+model<-train(label~.,data=train_Clin,method= "lm",  trControl=train.control)
+A1_model<-glm(label~.,data=train_Clin,family = binomial)
+train_Clin_pre<-predict(A1_model,newdata = train_Clin)
+train_Clin_roc<-roc(train_Clin$label,train_Clin_pre,ci=T)
+best=coords(train_Clin_roc,'best',ret='all',transpose=F)
+test_Clin_pre<-predict(A1_model,newdata = test_Clin,terms = 'response') 
+test_Clin_roc<-roc(test_Clin$label,test_Clin_pre,ci=T)
+best_2=coords(test_Clin_roc,'best',ret='all',transpose=F)
+Clin_model_train<-data.frame(
+  Type='Clin_model_train',
+  AUC=round(train_Clin_roc$auc,3),
+  '95% CI'=paste(round(train_Clin_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best$threshold,3),
+  spensitivity=round(best$specificity,3),
+  sensitivity=round(best$sensitivity,3),
+  accuracy=round(best$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+Clin_model_test<-data.frame(
+  Type='Clin_model_test',
+  AUC=round(test_Clin_roc$auc,3),
+  '95% CI'=paste(round(test_Clin_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best_2$threshold,3),
+  spensitivity=round(best_2$specificity,3),
+  sensitivity=round(best_2$sensitivity,3),
+  accuracy=round(best_2$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+
+#All_Radiomic----
+train_B1 <- df %>% 
+  select(label,contains('_original_')) 
+train_B1 <- train_B1[samples,]
+test_B1<-df %>% 
+  select(label,contains('_original_'))
+test_B1 <- test_B1[-samples,]
+train.control<-trainControl(method= "repeatedcv",  number= 10,repeats= 3)
+model<-train(label~.,data=train_B1,method= "lm",  trControl=train.control)
+B1_model<-glm(label~.,data=train_B1,family = binomial)
+train_B1_pre<-predict(B1_model,newdata = train_B1)
+train_B1_roc<-roc(train_B1$label,train_B1_pre,ci=T)
+best=coords(train_B1_roc,'best',ret='all',transpose=F)
+test_B1_pre<-predict(B1_model,newdata = test_B1,terms = 'response') 
+test_B1_roc<-roc(test_B1$label,test_B1_pre,ci=T)
+best_2=coords(test_B1_roc,'best',ret='all',transpose=F)
+B1_model_train<-data.frame(
+  Type='B1_model_train',
+  AUC=round(train_B1_roc$auc,3),
+  '95% CI'=paste(round(train_B1_roc$ci[c(1,3)],3),collapse =  '-'),
+  'Cutoff value'=round(best$threshold,3),
+  spensitivity=round(best$specificity,3),
+  sensitivity=round(best$sensitivity,3),
+  accuracy=round(best$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+B1_model_test<-data.frame(
+  Type='B1_model_test',
+  AUC=round(test_B1_roc$auc,3),
+  '95% CI'=paste(round(test_B1_roc$ci[c(1,3)],3),collapse =  '-'),
+  'Cutoff value'=round(best_2$threshold,3),
+  spensitivity=round(best_2$specificity,3),
+  sensitivity=round(best_2$sensitivity,3),
+  accuracy=round(best_2$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+
+#Combined_model----
+train_C1 <- df[samples,]
+test_C1<-df[-samples,]
+train.control<-trainControl(method= "repeatedcv",  number= 10,repeats= 3)
+model<-train(label~.,data=train_C1,method= "lm",  trControl=train.control)
+C1_model<-glm(label~.,data=train_C1,family = binomial)
+train_C1_pre<-predict(C1_model,newdata = train_C1)
+train_C1_roc<-roc(train_C1$label,train_C1_pre,ci=T)
+best=coords(train_C1_roc,'best',ret='all',transpose=F)
+test_C1_pre<-predict(C1_model,newdata = test_C1,terms = 'response') 
+test_C1_roc<-roc(test_C1$label,test_C1_pre,ci=T)
+best_2=coords(test_C1_roc,'best',ret='all',transpose=F)
+C1_model_train<-data.frame(
+  Type='C1_model_train',
+  AUC=round(train_C1_roc$auc,3),
+  '95% CI'=paste(round(train_C1_roc$ci[c(1,3)],3),collapse =  '-'),
+  'Cutoff value'=round(best$threshold,3),
+  spensitivity=round(best$specificity,3),
+  sensitivity=round(best$sensitivity,3),
+  accuracy=round(best$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+C1_model_test<-data.frame(
+  Type='C1_model_test',
+  AUC=round(test_C1_roc$auc,3),
+  '95% CI'=paste(round(test_C1_roc$ci[c(1,3)],3),collapse = '-'),
+  'Cutoff value'=round(best_2$threshold,3),
+  spensitivity=round(best_2$specificity,3),
+  sensitivity=round(best_2$sensitivity,3),
+  accuracy=round(best_2$accuracy,3),
+  RMSE=model$results[,2],
+  Rsquared=model$results[,3],
+  MAE=model$results[,4],
+  check.names = F)
+Combined_df<-rbind(AP_model_train,AP_model_test,
+                   PVP_model_train,PVP_model_test,
+                   T2_model_train,T2_model_test,
+                   Clin_model_train,Clin_model_test,
+                   B1_model_train,B1_model_test,
+                   C1_model_train,C1_model_test)
+fwrite(Combined_df,file = 'combined_df_model3.csv')
+
+library(maxLik)
+#C1_likehood_test
+predictions <- list(
+  modelAP1=AP_model,
+  modelPVP1=PVP_model,
+  ModelT2=T2_model,
+  ModelB1=B1_model,
+  ModelA1=A1_model
+)
+comparison_results <- data.frame(model1 = character(),
+                                 model2 = character(),
+                                 p.value = numeric(),
+                                 stringsAsFactors = FALSE)
+for (i in 1:5) {
+  pred1 <-  C1_model
+  pred2 <- predictions[[i]]
+  pvalue <- lrtest(pred1, pred2)$stats[3]
+  comparison_results[nrow(comparison_results) + 1,] <- c('Model C1', 
+                                                         names(predictions)[i], 
+                                                         pvalue)
+  
+}
+#B1_likehood_test
+predictions1 <- list(
+  'Model AP1'=AP_model,
+  'Model PVP1'=PVP_model,
+  'Model T2'=T2_model
+  
+)
+comparison_results1 <- data.frame(model1 = character(),
+                                  model2 = character(),
+                                  p.value = numeric(),
+                                  stringsAsFactors = FALSE)
+for (i in 1:3) {
+  pred1 <-  B1_model
+  pred2 <- predictions[[i]]
+  pvalue <- lrtest(pred1, pred2)$stats[3]
+  comparison_results[nrow(comparison_results) + 1,] <- c('Model B1', 
+                                                         names(predictions)[i], 
+                                                         pvalue)
+  
+}
+comparison_results1 <- rbind(comparison_results1,comparison_results)
+data.table::fwrite(comparison_results1,file = 'model3_likehold.csv')
+
+
+#---Train_AUC----
+pdf(file = 'Model3_Train_ROC.pdf',width =6,height = 5)
+plot(train_AP_roc, col="#1C9A35", lwd=2, title = "")
+plot(train_PVP_roc, col="#1B4E8C", lwd=2, add = T)
+plot(train_T2_roc, col="#EDB85D", lwd=2, add = T)
+plot(train_B1_roc, col="#6595A3", lwd=2, add = T)
+plot(train_Clin_roc, col="#555555", lwd=2, add = T)
+plot(train_C1_roc, col="#E41A1C", lwd=2, add = T)
+legend("bottomright",
+       c(paste0("Model AP3 AUC=",round(train_AP_roc$auc,3)), 
+         paste0("Model PVP3 AUC=",round(train_PVP_roc$auc,3)), 
+         paste0("Model T2-3 AUC=",round(train_T2_roc$auc,3)),
+         paste0("Model B3 AUC=",round(train_B1_roc$auc,3)), 
+         paste0("Model A3 AUC=",round(train_Clin_roc$auc,3)), 
+         paste0("Model C3 AUC=",round(train_C1_roc$auc,3))
+       ),
+       col=c("#1C9A35", "#1B4E8C", "#EDB85D", "#6595A3","#555555","#E41A1C"),
+       lty=1, lwd=3,bty = "n"
+)
+mtext("ROC curves of given models", line=2)
+dev.off()
+#---Test_AUC----
+pdf(file = 'Model3_Test_ROC.pdf',width = 6,height = 5)
+plot(test_AP_roc, col="#1C9A35", lwd=2, title = "")
+plot(test_T2_roc, col="#1B4E8C", lwd=2, add = T)
+plot(test_PVP_roc, col="#EDB85D", lwd=2, add = T)
+plot(test_B1_roc, col="#6595A3", lwd=2, add = T)
+plot(test_Clin_roc, col="#555555", lwd=2, add = T)
+plot(test_C1_roc, col="#E41A1C", lwd=2, add = T)
+legend("bottomright",
+       c(paste0("Model AP3 AUC=",round(test_AP_roc$auc,3)), 
+         paste0("Model PVP3 AUC=",round(test_PVP_roc$auc,3)), 
+         paste0("Model T2-3 AUC=",round(test_T2_roc$auc,3)),
+         paste0("Model B3 AUC=",round(test_B1_roc$auc,3)), 
+         paste0("Model A3 AUC=",round(test_Clin_roc$auc,3)), 
+         paste0("Model C3 AUC=",round(test_C1_roc$auc,3))
+       ),
+       col=c("#1C9A35", "#1B4E8C", "#EDB85D", "#6595A3","#555555","#E41A1C"),
+       lty=1, lwd=2,bty = "n"
+)
+mtext("ROC curves of given models", line=2)
+dev.off()
+}
+
+#---DCA----
+library(ggDCA)
+library(ggpubr)
+dd<-datadist(train_C1)
+options(datadist='dd')
+dca12 <- ggDCA::dca(C1_model,B1_model,A1_model,AP_model,PVP_model,T2_model)
+dca_df <- as.data.frame(dca12)
+dca_df$model <- as.character(dca_df$model)
+dca_df$model[dca_df$model=='AP_model']='Model AP3' 
+dca_df$model[dca_df$model=='PVP_model']='Model PVP3' 
+dca_df$model[dca_df$model=='T2_model']='Model T2-3' 
+dca_df$model[dca_df$model=='B1_model']='Model B3' 
+dca_df$model[dca_df$model=='A1_model']='Model A3' 
+dca_df$model[dca_df$model=='C1_model']='Model C3' 
+
+ggplot(data = dca_df, aes(x = thresholds, y = NB)) +
+  geom_point(colour='white',aes(color = model)) +
+  scale_color_manual(values = c("#1C9A35", "#1B4E8C", "#EDB85D", "#6595A3","#555555","#E41A1C","#348AA6FF","#A0DFB9FF"),
+                     limits = c('Model AP3','Model PVP3','Model T2-3',
+                                'Model B3','Model A3','Model C3',
+                                'All','None')) +
+  geom_smooth(aes(color = model), method = 'loess', se = F, show.legend = T) +
+  theme_bw() +
+  theme(legend.position = c(0.88,0.76),
+        legend.background = element_blank(),
+        legend.title = element_blank(),
+        panel.background = element_blank(),
+        panel.grid = element_blank(),
+        plot.title = element_text(vjust = 0,hjust = 0.5),
+        axis.title = element_text(face = 'plain',colour = 'black',size = 18),
+        axis.text = element_text(face = 'plain',colour = 'black',size = 12)) +
+  labs(title = 'Decision curve of given models',x = 'probability Threshold', y = 'Net Benefit')+
+  coord_cartesian(ylim =c(-0.15,0.3), xlim =c(0,1))
+ggsave(filename = 'Model3_DCA.pdf',height = 5,width = 6)
+
+#---ca
+dd<-datadist(train_C1)
+options(datadist='dd')
+fit<-lrm(label~.,data=train_C1,x=T,y=T)
+cal2 <- calibrate(fit, method = 'boot', B = 1000)
+pdf(file = 'Model3_calibrate_curve.pdf',width = 5,height = 5)
+plot(cal2,
+     xlim = c(0,1),
+     ylim = c(0,1),
+     xlab = "Prediced Probability",
+     ylab = "Observed Probability",
+     cex.lab=1.2, cex.axis=1, cex.main=1.2, cex.sub=0.8,
+     #subtitles = FALSE,
+     legend = FALSE
+) 
+lines(cal2[,c("predy","calibrated.corrected")], 
+      type = 'l', #连线的类型，可以是"p","b","o"
+      lwd = 3, #连线的粗细
+      pch = 16, #点的形状，可以是0-20
+      col = "#2166AC") #连线的颜色
+lines(cal2[,c("predy","calibrated.orig")],type="l",pch=16,lwd=3,col="tomato")
+abline(0,1,
+       lty = 2, #对角线为虚线
+       lwd = 2, #对角线的粗细
+       col = "#224444") #对角线的颜色
+legend(0.6,0.2,
+       c("Apparent","Bias-corrected","Ideal"), 
+       lty = c(2,1,1), 
+       lwd = c(2,3,3), 
+       col = c("black","#2166AC","tomato"), 
+       bty = "n"
+)
+dev.off()
